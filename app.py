a9d3bbe751bc85fb416462048241d9c009090798
Umutambyi Gad <umutambyig@gmail.com>
2021-03-16 19:40:35 +0100

    Add an argument that will allow you to shutdown computer when the process is over

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from pytube import YouTube
 import os
 import time
-
+import platform
 
 class Automate:
 	def __init__(self, *urls):
@@ -20,7 +20,15 @@ class Automate:
 				collections.append(url.strip())
 		return collections
 
-	def download(self, after=0, dir=os.path.dirname(__file__)):
+	def download(self, after=0, dir=os.path.dirname(__file__), shutdown=False):
 		time.sleep(after)
 		for url in Automate.playList(self.urls):
 			YouTube(url).youtube.streams.first().download(dir)
+		if shutdown:
+			if platform.system() == 'Windows':
+				os.system('shutdown /s /t 1')
+			elif platform.system() == 'Linux':
+				os.system('shutdown now -h') # notice that you have root privileges
+			elif platform.system() == 'Darwin':
+				os.system('shutdown -h now') # notice that you have root privileges
+
