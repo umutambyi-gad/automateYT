3656dc103770401097db9dbc2281f9423dddff24
Umutambyi Gad <umutambyig@gmail.com>
2021-03-16 18:50:00 +0100

    Define class Automate for automating download

---
+++ app.py
@@ -0,0 +1,26 @@
+from pytube import YouTube
+import os
+import time
+
+
+class Automate:
+	def __init__(self, *urls):
+		self.urls = urls
+		if not len([i for i in self.urls if i]) > 0:
+			raise Exception("List of urls can not be empty")
+	
+	@classmethod
+	def playList(cls, urls):
+		collections = []
+		for url in urls:
+			if isinstance(url, tuple) or isinstance(url, list):
+				for nested in url:
+					collections.append(nested.strip())
+			else:
+				collections.append(url.strip())
+		return collections
+
+	def download(self, after=0, dir=os.path.dirname(__file__)):
+		time.sleep(after)
+		for url in Automate.playList(self.urls):
+			YouTube(url).youtube.streams.first().download(dir)
