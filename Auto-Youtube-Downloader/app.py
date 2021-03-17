f58c61a123bf3ca17aea0649e93a9896e9a42e7a
Umutambyi Gad <umutambyig@gmail.com>
2021-03-17 14:34:11 +0100

    Add documentation to Automate class and its methods

--- Auto-Youtube-Downloader/app.py
+++ Auto-Youtube-Downloader/app.py
@@ -9,11 +9,20 @@ from exceptions import (
 
 
 class Automate:
+	"""class for automating youtube videos downloading"""
 	def __init__(
 		self,
 		*urls: list or tuple,
 		**urls_with_res: dict
 	):
+		"""Construct a :class:`Automate <Automate>`.
+
+        :param list or tuple urls:
+            valid list or tuple of YouTube watch URL.
+        :param dict urls_with_res:
+            dict where keys are valid YouTube watch URL and values are valid resolution.
+
+        """
 		self.urls = urls
 		self.urls_with_res = urls_with_res
 
@@ -22,6 +31,14 @@ class Automate:
 	
 	
 	def __playList(self, urls: list or tuple) -> list:
+		"""Convert (tuple or list) or nested tuple of videos into list.
+
+		:param tuple or list urls
+			list or tuple of YouTube watch URL they can be nested or not
+
+        :rtype: list
+
+        """
 		collections = []
 		for url in urls:
 			if isinstance(url, tuple) or isinstance(url, list):
@@ -39,6 +56,22 @@ class Automate:
 		highest_res=True,
 		lowest_res=False
 	) -> None:
+		"""Function for downloading YouTube videos
+
+		:param int after
+			number of seconds to delay before download
+		:param str location
+			location on your computer to save the downloads
+		:param bool shutdown
+			if shutdown is True computer shuts down after downloading
+		:param bool highest_res
+			if highest_res is True the script downloads highest resolution
+		:param bool lowest_res
+			if lowest_res is True the script downloads lowest resolution
+
+        :rtype: None
+
+        """
 		if highest_res:
 			lowest_res = False
 		elif lowest_res:
