7062e2bc9f1f7fcf35c88e0c63e3e5d7f7dbe425
Umutambyi Gad <umutambyig@gmail.com>
2021-03-21 20:21:05 +0000

    Export from one directory

--- Automate/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Python library for automating downloading YouTube Videos.
-"""
-
-__title__ = "automate"
-__author__ = "Umutambyi Gad"
-__copyright__ = "Copyright 2021 Umutambyi Gad"
-
-from core import Automate
