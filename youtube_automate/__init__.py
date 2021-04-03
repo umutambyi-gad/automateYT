c46883ac740ee72089128c0a26b0cc6197c4766c
Umutambyi Gad <umutambyig@gmail.com>
2021-04-03 22:45:41 +0000

    Edit in copyright

--- youtube_automate/__init__.py
+++ youtube_automate/__init__.py
@@ -4,7 +4,7 @@ Python library for automating downloading YouTube Videos.
 
 __title__ = "automate"
 __author__ = "Umutambyi Gad"
-__copyright__ = "Copyright 2021 Umutambyi Gad"
+__copyright__ = "Copyright 2021 by Umutambyi Gad"
 
 from youtube_automate.__main__ import Automate
 from youtube_automate.__main__ import TimeFormatting
