92b4df3342b7ca2f68c6c32b682dd7fa00332fa4
Umutambyi Gad <umutambyig@gmail.com>
2021-04-15 15:09:25 +0000

    Test if it raises EmptyLookUpListError when constructor is required by not given

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -1,1 +1,25 @@
 import pytest
+from autoYT.__main__ import Automate
+from autoYT.exceptions import *
+
+class TestExceptions:
+	def test_EmptyLookUpListError(self):
+		with pytest.raises(EmptyLookUpListError) as exc_info:
+			Automate().info()
+
+		assert str(exc_info.value) == "List or dict of (videos or audios) to access can not be empty"
+
+		with pytest.raises(EmptyLookUpListError) as exc_info:
+			Automate(urls_with_res={}).download()
+
+		assert str(exc_info.value) == "List or dict of (videos or audios) to access can not be empty"
+
+		with pytest.raises(EmptyLookUpListError) as exc_info:
+			Automate().download_subtitle()
+
+		assert str(exc_info.value) == "List or dict of (videos or audios) to access can not be empty"
+
+		with pytest.raises(EmptyLookUpListError) as exc_info:
+			Automate(urls_with_res={}).download_playlist()
+
+		assert str(exc_info.value) == "List or dict of (videos or audios) to access can not be empty"
