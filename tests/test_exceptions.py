dc7155e19a62c562430116849e2e694c7330a5b7
Umutambyi Gad <umutambyig@gmail.com>
2021-04-15 15:33:51 +0000

    Test if it raises TypeError when wrong datatype is given as constructor

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -24,7 +24,7 @@ class TestExceptions:
 
 		assert str(exc_info.value) == "List or dict of (videos or audios) to access can not be empty"
 
-	@pytest.mark.this
+
 	def test_ResolutionAbsenceError(self):
 		with pytest.raises(ResolutionAbsenceError) as exc_info:
 			Automate('https://www.youtube.com/watch?v=XqZsoesa55w').download(highest_res=False, lowest_res=False)
@@ -35,3 +35,26 @@ class TestExceptions:
 			Automate('https://www.youtube.com/playlist?list=PLS1QulWo1RIaJECMeUT4LFwJ-ghgoSH6n').download_playlist(highest_res=False, lowest_res=False)
 
 		assert str(exc_info.value) == "Neither highest nor lowest resolution specified"
+
+
+	def test_NonExistLocationError(self):
+		with pytest.raises(NonExistLocationError) as exc_info:
+			Automate('https://www.youtube.com/watch?v=XqZsoesa55w').download(location='C:/someNonExistPath')
+
+		assert str(exc_info.value) == "provided location (path) doesn't exists"
+
+		with pytest.raises(NonExistLocationError) as exc_info:
+			Automate('https://www.youtube.com/playlist?list=PLS1QulWo1RIaJECMeUT4LFwJ-ghgoSH6n').download_playlist(location='home/someNonExistPath')
+
+		assert str(exc_info.value) == "provided location (path) doesn't exists"
+
+	def test_TypeError(self):
+		with pytest.raises(TypeError) as exc_info:
+			Automate({'https://www.youtube.com/watch?v=XqZsoesa55w'}).download()
+
+		assert str(exc_info.value) == "tuple, list or string were expected but set was given"
+
+		with pytest.raises(TypeError) as exc_info:
+			Automate(urls_with_res='https://www.youtube.com/playlist?list=PLS1QulWo1RIaJECMeUT4LFwJ-ghgoSH6n').download_playlist()
+
+		assert str(exc_info.value) == "dict were expected but str was given"
