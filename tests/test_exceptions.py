3ed8cf4cacd072f5b8d3f19768ee2814fb8595da
Umutambyi Gad <umutambyig@gmail.com>
2021-04-15 15:19:34 +0000

    Test if it raises ResolutionAbsenceError when none of lowest_res or highest_res is set to True

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -23,3 +23,15 @@ class TestExceptions:
 			Automate(urls_with_res={}).download_playlist()
 
 		assert str(exc_info.value) == "List or dict of (videos or audios) to access can not be empty"
+
+	@pytest.mark.this
+	def test_ResolutionAbsenceError(self):
+		with pytest.raises(ResolutionAbsenceError) as exc_info:
+			Automate('https://www.youtube.com/watch?v=XqZsoesa55w').download(highest_res=False, lowest_res=False)
+
+		assert str(exc_info.value) == "Neither highest nor lowest resolution specified"
+
+		with pytest.raises(ResolutionAbsenceError) as exc_info:
+			Automate('https://www.youtube.com/playlist?list=PLS1QulWo1RIaJECMeUT4LFwJ-ghgoSH6n').download_playlist(highest_res=False, lowest_res=False)
+
+		assert str(exc_info.value) == "Neither highest nor lowest resolution specified"
