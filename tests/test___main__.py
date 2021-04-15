d43ad395fff969b58d2045992c085052da8444b7
Umutambyi Gad <umutambyig@gmail.com>
2021-04-15 12:09:08 +0000

    Add fixture for the all test methods

--- tests/test___main__.py
+++ tests/test___main__.py
@@ -1,1 +1,25 @@
 import pytest
+from autoYT.__main__ import Automate
+import math
+import time
+import json
+import yaml
+import os
+import requests
+
+
+class TestMain:
+	@pytest.fixture
+	def automate(self):
+		urls = (
+			'https://www.youtube.com/watch?v=vFWv44Z4Jhk',
+			'https://www.youtube.com/watch?v=Kx68g1rLbbU'
+		)
+
+		urls_with_res = {
+			'https://www.youtube.com/watch?v=TgItkJCm09c': '144p',
+		}
+
+		return Automate(urls, urls_with_res=urls_with_res)
+
+	
