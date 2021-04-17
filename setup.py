50c4f9b0cd3cc2e979d50ea8cba9cd97782d5c84
Umutambyi Gad <umutambyig@gmail.com>
2021-04-17 23:37:06 +0000

    add python_requires argument on setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,11 @@
 import pathlib
 from setuptools import setup
 
-# The directory containing this file
+
 HERE = pathlib.Path(__file__).parent
 
-# The text of the README file
 README = (HERE / "README.md").read_text()
 
-# This call to setup() does all the work
 setup(
     name="autoYT",
     version="1.0.0",
@@ -18,14 +16,22 @@ setup(
     author="Umutambyi Gad",
     author_email="umutambyig@gmail.com",
     license="MIT",
+    python_requires=">=3.6",
+    entry_points={
+        'console_scripts': [
+            'autoYT = autoYT:main',
+        ]
+    },
     classifiers=[
         "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python",
     ],
     packages=["autoYT"],
     include_package_data=True,
     install_requires=["pytube"],
+    keywords=["automate", "youtube", "download", "download_playlist", "download_subtitle", "generate_watch_url_from_playlist",],
 )
