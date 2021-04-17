eaa44a8b01e8625608d92c62fa18ce8b25102fd2
Umutambyi Gad <umutambyig@gmail.com>
2021-04-17 13:11:15 +0000

    List all available classes in __all__ metadata

--- autoYT/__init__.py
+++ autoYT/__init__.py
@@ -1,5 +1,5 @@
 """
-Python library for automating downloading YouTube Videos.
+autoYT is lightweight library for automating to download youtube videos, subtitles (if available) and playlist.
 """
 
 __title__ = "autoYT"
@@ -8,6 +8,7 @@ __copyright__ = "Copyright 2021 by Umutambyi Gad"
 __license__ = 'MIT'
 __version__ = '1.0.0'
 
+__all__ = ['Timing', 'Automate']
 
 from autoYT.__main__ import Automate
 from autoYT.__main__ import Timing
